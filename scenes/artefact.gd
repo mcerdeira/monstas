4066bb2d95bc1a2b47292ebf1c7b40f3379b217d
Martin Cerdeira <martincerdeira@gmail.com>
2026-03-15 11:28:01 -0300

    Cambios

--- scenes/artefact.gd
+++ scenes/artefact.gd
@@ -7,29 +7,11 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("left"):
 			animating = true
 			var tween = get_tree().create_tween()
-
-			var val = -166
-			if %shoot_line.points[1].y == -166:
-				val = -200
-				
-			var p = %shoot_line.get_point_position(1)
-			var target = Vector2(p.x, -200)
-			tween.tween_method(set_line_point, p, target, duration)
-				
 			tween.tween_property(self, "rotation_degrees", rotation_degrees - 45, duration)
 			tween.tween_callback(_ended)
 		elif Input.is_action_just_pressed("right"):
 			animating = true
 			var tween = get_tree().create_tween()
-
-			var val = -166
-			if %shoot_line.points[1].y == -166:
-				val = -200
-				
-			var p = %shoot_line.get_point_position(1)
-			var target = Vector2(p.x, -200)
-			tween.tween_method(set_line_point, p, target, duration)
-			
 			tween.tween_property(self, "rotation_degrees", rotation_degrees + 45, duration)
 			tween.tween_callback(_ended)
 
