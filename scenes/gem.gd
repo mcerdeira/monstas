d9dc4d7acfb77600da17b18bc979bd829bece8a0
Martin Cerdeira <martincerdeira@gmail.com>
2026-03-16 00:47:10 -0300

    algun avance

--- scenes/gem.gd
+++ scenes/gem.gd
@@ -1,7 +1,9 @@
 extends AnimatedSprite2D
+var monsta = null
 
-func _ready() -> void:
-	%monster.play(Global.pick_random(["fish", "poop", "zombie"]))
+func set_monsta(_monsta):
+	monsta = _monsta
+	%monster.play(_monsta)
 
 func _physics_process(delta: float) -> void:
 	rotation_degrees = -get_parent().rotation_degrees
