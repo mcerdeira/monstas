bcde15c7aa68da83d6231d87cbe1af286f00699b
Martin Cerdeira <martincerdeira@gmail.com>
2026-03-16 23:58:25 -0300

    cierre de ciclo

--- scenes/gem.gd
+++ scenes/gem.gd
@@ -1,17 +1,38 @@
 extends AnimatedSprite2D
 var monsta = null
+var temp_monsta = null
+var minibullet_obj = load("res://scenes/minibullet.tscn")
+
+func set_monsta_callback():
+	var minibullet = minibullet_obj.instantiate()
+	minibullet.monsta = monsta
+	minibullet.origin = $clickeable
+	minibullet.global_position = global_position
+	get_parent().get_parent().add_child(minibullet)
+	$AnimationPlayer.stop()
+	set_monsta(temp_monsta)
+	temp_monsta = null
+	$monster/stars.visible = false
 
 func set_monsta(_monsta):
 	monsta = _monsta
+	%monster_temp.animation = monsta.id
 	%monster.play(_monsta.id)
 
 func _physics_process(delta: float) -> void:
 	rotation_degrees = -get_parent().rotation_degrees
 
 func hit(_monsta):
-	if monsta == null:
+	if monsta != null:
+		%monster_temp.animation = $monster.animation
+		$monster/stars.visible = true
+		temp_monsta = _monsta
+		%monster_temp.animation = temp_monsta.id
+		%monster_temp.visible = true
+		$AnimationPlayer.play("jump")
+	else:
 		set_monsta(_monsta)
-
+	
 func _on_clickeable_mouse_entered() -> void:
 	if monsta:
 		%Ballon.set_text(monsta, %monster.global_position)
---
+++ scenes/minibullet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+var monsta = null
+var origin = null
+var speed = 800
+
+func _physics_process(delta: float) -> void:
+	if monsta:
+		global_position.y -= speed * delta
+		if global_position.y <= -150:
+			queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if monsta:
+		if area and origin != area and area.is_in_group("gem"):
+			area.hit(monsta)
+			monsta = null
+			queue_free()
